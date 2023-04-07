18e532720bfcaff52b74f8bd009f202ac03e19bb
Fernando Fukunaga <fanna@brasilseg.com.br>
2023-04-07 12:38:33 -0300

    adicionando o Bearer no começo do token

--- main.py
+++ main.py
@@ -13,19 +13,20 @@ def login(username, password):
         hashed_password_from_db = database[username]
         if bcrypt.checkpw(password.encode(), hashed_password_from_db):
             token = jwt.encode({"username": username}, SECRET_KEY, algorithm='HS256')
-            return token
+            return "Bearer " + token
         else:
             return "400 bad request"
     return "400 bad request"
 
 register_user("fernando", "senha123")
-token = login("fernando", "senha13")
+token = login("fernando", "senha123")
 
 if token == "400 bad request":
     raise TypeError(token + ": usuario ou senha incorretos!!")
 
 def request(token):
-    token_decoded = jwt.decode(token, SECRET_KEY, algorithms="HS256")
+    token_stripped = token.replace("Bearer ", "")
+    token_decoded = jwt.decode(token_stripped, SECRET_KEY, algorithms="HS256")
     return token_decoded
     
 response = request(token)
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 ## Resumo
 Este script serve como templete para um fluxo bem simples de autorização com conceitos de oauth2. Ele registra um usuário utilizando o bcrypt para hashear a senha, depois, tenta fazer o login usando a senha normal e conflitando com a senha hasehada do banco de dados usando a função bcrypt.checkpw(). E por fim, se tudo estiver correto, ele gera um token JWT e tenta usá-lo para fazer uma requisição.
+
+A requisição deve retornar um dict com o próprio username do usuário.
