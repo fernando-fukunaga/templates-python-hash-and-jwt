331fc17c1271c8bb54c094865c8dc9eaa2126b1c
Fernando Fukunaga <fanna@brasilseg.com.br>
2023-04-06 23:38:56 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+Ordem das tarefas:
+
+1 - Criar um Sign Up
+2 - Criptografar a senha inputada
+3 - Guardar dados em um db sintético que será um dict
+4 - Criar um Sign In
+5 - Confrontar a senha inserida no Sign In com a hasheada do db
+6 - Se der certo, gerar um token jwt com o username do user
+7 - Usar esse token para algo
+'''
